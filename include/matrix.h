820cf65954f9cc398c0a4274460d5c78f634330c
Den-Git-CPP <madim-neodart@rambler.ru>
2022-05-29 11:02:46 +0300

    Реализация матрицы

---
+++ include/matrix.h
@@ -0,0 +1,95 @@
+#pragma once
+#include <tuple>
+#include <map>
+
+template<typename T, T default_value>
+class Matrix {
+	using storage = std::map<std::tuple<std::size_t, std::size_t>, T>;
+
+	class Element {
+	public:
+		Element(storage& data, std::size_t row, std::size_t col) :
+			_data(data), _row(row), _col(col) {};
+		operator T() {
+			if (_data.count({ _row, _col })) {
+				return _data[{_row, _col}];
+			}
+			else {
+				return default_value;
+			}
+		}
+		operator const T() const {
+			if (_data.count({ _row, _col })) {
+				return _data[{_row, _col}];
+			}
+			else {
+				return default_value;
+			}
+		}
+		Element& operator=(T element) {
+			if (element != default_value)
+				_data[{_row, _col}] = element;
+			else
+				_data.erase({ _row,_col });
+			return *this;
+		};
+	private:
+		std::size_t _row, _col;
+		storage& _data;
+	};
+
+	class Row {
+	public:
+		Row(storage& data, std::size_t row) : _data(data), _row(row) {};
+		Element operator[](std::size_t columnindex) {
+			return Element(_data, _row, columnindex);
+		};
+	private:
+		std::size_t _row;
+		storage& _data;
+	};
+
+public:
+	class iterator : public std::iterator<std::forward_iterator_tag, T> {
+	public:
+		iterator(typename storage::iterator iter)
+			: it(iter) {};
+		bool operator==(iterator& rhs) {
+			return it == rhs.it;
+		}
+		bool operator!=(iterator& rhs) {
+			return it != rhs.it;
+		}
+		iterator& operator--() {
+			--it;
+			return *this;
+		}
+		iterator& operator++() {
+			++it;
+			return *this;
+		}
+		std::tuple<std::size_t, std::size_t, T> operator*() {
+			return {
+				std::get<0>(it->first),
+				std::get<1>(it->first),
+				it->second };
+		}
+	protected:
+		typename storage::iterator it;
+	};
+
+	std::size_t size() {
+		return _data.size();
+	};
+	Row operator[](std::size_t index) {
+		return Row(_data, index);
+	};
+	Matrix::iterator begin() {
+		return _data.begin();
+	};
+	Matrix::iterator end() {
+		return _data.end();
+	};
+private:
+	storage _data;
+};
--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,55 @@
-﻿#include <iostream>
+﻿#include "matrix.h"
+
+#include <iostream>
+#include <cassert>
 
 int main() {
+	 // бесконечная матрица int заполнена значениями -1 пример ДЗ
+	Matrix<int, -1> matrix;
+	assert(matrix.size() == 0); // все ячейки свободны
+	auto a = matrix[0][0];
+	assert(a == -1);
+	assert(matrix.size() == 0);
+	matrix[100][100] = 314;
+	assert(matrix[100][100] == 314);
+	assert(matrix.size() == 1);
+	// выведется одна строка
+	// 100100314
+	for (auto c : matrix)
+	{
+		int x;
+		int y;
+		int v;
+		std::tie(x, y, v) = c;
+		std::cout << x << y << v << "\n";
+	}
+	
+	//матрица заполнения 
+	int n = 10;
+	Matrix<int, 0> matrix_2;
+	for (int i = 0; i < n; ++i) {
+		matrix_2[i][i] = i;
+	}
+	for (int i = 0; i < n; ++i) {
+		matrix_2[i][(n - 1) - i] = (n - 1) - i;
+	}
+	std::cout << "\nSparce Matrix\n";
+	for (int i = 1; i < n - 1; ++i) {
+		for (int j = 1; j < n - 1; ++j) {
+			std::cout << matrix_2[i][j] << ' ';
+		}
+		std::cout << "\n";
+	}
+	std::cout << "\nSparce Matrix size() = " << matrix_2.size() << "\n";
+	std::cout << "\n";
 
-std:: cout<<"Hello Matrix";
-return 0;
+	for (auto elem : matrix_2) {
+		int x;
+		int y;
+		int value;
+		std::tie(x, y, value) = elem;
+		std::cout << "matrix[" << x << "][" << y << "] = " << value <<"\n";
+	}
+
+	return 0;
 }
